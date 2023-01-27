1ae6d5fb785b96cd3e7dce6c24be89a6db1b5a05
duduaja <duajaadiwijayaa@gmail.com>
2023-01-27 08:50:17 +0700

    try deploy on streamlit

--- clickbait.py
+++ clickbait.py
@@ -144,3 +144,27 @@ def predict_spam(predict_msg):
                       padding = padding_type,
                       truncating=trunc_type)
     return (model.predict(padded))
+
+
+
+#DEPLOY ON STREAMLIT
+import streamlit as st
+
+
+def main():
+    st.title('Clickbait Classification')
+
+    #input
+    text_input = st.text_input('Headline News')
+
+    #prediction
+    if st.button('Predict'):
+        list_text_input = []
+        list_text_input.append(text_input)
+        pred_result = predict_spam(list_text_input)[0][0]
+        # st.success('{headline} \n**{pred_result:.2%}** clickbait'.format(headline=text_input,pred_result=pred_result))
+
+        st.markdown('<p style="font-size:50px">{pred_result:.2%} clickbait</p>'.format(pred_result=pred_result), unsafe_allow_html=True)
+
+if __name__=='__main__':
+    main()
