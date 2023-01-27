89f719b8f7f0c25869ddc5ae6ac15337f5492533
duduaja <duajaadiwijayaa@gmail.com>
2023-01-27 08:39:27 +0700

    minor edit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from clickbait import *
 coba_text=['Viral Preman Palak Ibu Penjual Mie Pecal di Medan, Pelaku Ditangkap',
            'Terkuak Alasan Jaksa Sebut Bharada Eliezer Bukan Justice Collaborator',
            'Fakta-fakta Pembunuhan Berantai Wowon Cs di Bekasi-Cianjur',
@@ -5,9 +6,5 @@ coba_text=['Viral Preman Palak Ibu Penjual Mie Pecal di Medan, Pelaku Ditangkap'
            'UU No. 6 Tahun 2014 soal Desa Penambahan Masa Jabatan Kepala Desa Hanya Melanggengkan Oligarki',
            'Menag Usulkan Biaya Haji Naik Jadi Rp69 Juta untuk Jemaah']
 
-# print(predict_spam(coba_text))
-# for i in coba_text:
-#     hasil = predict_spam(i)[0][0]
-#     print(hasil)
 
-print('Viral Preman Palak Ibu Penjual Mie Pecal di Medan, Pelaku Ditangkap\nTerkuak Alasan Jaksa Sebut Bharada Eliezer Bukan Justice Collaborator')
+print(predict_spam(coba_text))
